65e1ac72533b5044ec06b7a6d43ed4a709e2d578
Rahmanism <rahmani@mailkh.com>
2020-07-08 14:54:37 +0430

    Initial commit.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Excel2Csv
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.inputPanel = new System.Windows.Forms.Panel();
+            this.aboutBtn = new System.Windows.Forms.Button();
+            this.clearListBtn = new System.Windows.Forms.Button();
+            this.outputLbl = new System.Windows.Forms.Label();
+            this.fileListLbx = new System.Windows.Forms.ListBox();
+            this.messageLbl = new System.Windows.Forms.Label();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.bottomPnl = new System.Windows.Forms.Panel();
+            this.inputPanel.SuspendLayout();
+            this.bottomPnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputPanel
+            // 
+            this.inputPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.inputPanel.Controls.Add(this.aboutBtn);
+            this.inputPanel.Controls.Add(this.clearListBtn);
+            this.inputPanel.Controls.Add(this.outputLbl);
+            this.inputPanel.Controls.Add(this.fileListLbx);
+            this.inputPanel.Controls.Add(this.messageLbl);
+            this.inputPanel.Location = new System.Drawing.Point(0, 0);
+            this.inputPanel.Name = "inputPanel";
+            this.inputPanel.Size = new System.Drawing.Size(800, 403);
+            this.inputPanel.TabIndex = 0;
+            // 
+            // aboutBtn
+            // 
+            this.aboutBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.aboutBtn.AutoSize = true;
+            this.aboutBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.aboutBtn.Location = new System.Drawing.Point(752, 7);
+            this.aboutBtn.Name = "aboutBtn";
+            this.aboutBtn.Size = new System.Drawing.Size(34, 31);
+            this.aboutBtn.TabIndex = 0;
+            this.aboutBtn.Text = "?";
+            this.aboutBtn.Click += new System.EventHandler(this.aboutBtn_Click);
+            // 
+            // clearListBtn
+            // 
+            this.clearListBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.clearListBtn.AutoSize = true;
+            this.clearListBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clearListBtn.Location = new System.Drawing.Point(19, 361);
+            this.clearListBtn.Name = "clearListBtn";
+            this.clearListBtn.Size = new System.Drawing.Size(99, 31);
+            this.clearListBtn.TabIndex = 0;
+            this.clearListBtn.Text = "Clear &list";
+            this.clearListBtn.Click += new System.EventHandler(this.clearListBtn_Click);
+            // 
+            // outputLbl
+            // 
+            this.outputLbl.AutoSize = true;
+            this.outputLbl.Location = new System.Drawing.Point(361, 57);
+            this.outputLbl.Name = "outputLbl";
+            this.outputLbl.Size = new System.Drawing.Size(308, 21);
+            this.outputLbl.TabIndex = 2;
+            this.outputLbl.Text = "Click on Convert button to start converting.";
+            // 
+            // fileListLbx
+            // 
+            this.fileListLbx.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.fileListLbx.FormattingEnabled = true;
+            this.fileListLbx.HorizontalScrollbar = true;
+            this.fileListLbx.ItemHeight = 21;
+            this.fileListLbx.Location = new System.Drawing.Point(19, 57);
+            this.fileListLbx.Name = "fileListLbx";
+            this.fileListLbx.Size = new System.Drawing.Size(325, 298);
+            this.fileListLbx.TabIndex = 1;
+            // 
+            // messageLbl
+            // 
+            this.messageLbl.AutoSize = true;
+            this.messageLbl.Location = new System.Drawing.Point(19, 17);
+            this.messageLbl.Name = "messageLbl";
+            this.messageLbl.Size = new System.Drawing.Size(265, 21);
+            this.messageLbl.TabIndex = 0;
+            this.messageLbl.Text = "Drag Excel files and drop them here...";
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveBtn.AutoSize = true;
+            this.saveBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saveBtn.Location = new System.Drawing.Point(689, 5);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(99, 31);
+            this.saveBtn.TabIndex = 0;
+            this.saveBtn.Text = "&Convert";
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // bottomPnl
+            // 
+            this.bottomPnl.Controls.Add(this.saveBtn);
+            this.bottomPnl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottomPnl.Location = new System.Drawing.Point(0, 409);
+            this.bottomPnl.Name = "bottomPnl";
+            this.bottomPnl.Size = new System.Drawing.Size(800, 41);
+            this.bottomPnl.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bottomPnl);
+            this.Controls.Add(this.inputPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Excel2Csv";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            this.DragLeave += new System.EventHandler(this.MainForm_DragLeave);
+            this.inputPanel.ResumeLayout(false);
+            this.inputPanel.PerformLayout();
+            this.bottomPnl.ResumeLayout(false);
+            this.bottomPnl.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel inputPanel;
+        private System.Windows.Forms.Panel bottomPnl;
+        private System.Windows.Forms.Label messageLbl;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.ListBox fileListLbx;
+        private System.Windows.Forms.Label outputLbl;
+        private System.Windows.Forms.Button clearListBtn;
+        private System.Windows.Forms.Button aboutBtn;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excel2Csv
+{
+    public partial class MainForm : Form
+    {
+        List<string> fileList;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            fileList = new List<string>();
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            inputPanel.BackColor = Color.Red;
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void MainForm_DragLeave(object sender, EventArgs e)
+        {
+            inputPanel.BackColor = Control.DefaultBackColor;
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            inputPanel.BackColor = Control.DefaultBackColor;
+            messageLbl.Text = "";
+            foreach (string path in (string[])e.Data.GetData(DataFormats.FileDrop))
+            {
+                fileList.Add(Path.GetFullPath(path));
+            }
+            fileListLbx.Items.AddRange(fileList.ToArray());
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            bool successful = true;
+            foreach (var xl in fileList) {
+                successful = Excel2CsvConvertor.SaveAsCsv(xl);
+            }
+            messageLbl.Text = successful
+                ? "Files are saved to the same directory."
+                : "Error converting files.";
+        }
+
+        private void clearListBtn_Click(object sender, EventArgs e)
+        {
+            fileListLbx.Items.Clear();
+            fileList.Clear();
+        }
+
+        private void aboutBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Excel to CSV convertor.\n" +
+                "\nRahmanism\n" +
+                "https://github.com/Rahmanism/Excel2Csv" +
+                "\n\nVersion: 1.0.0\n" +
+                "July 8, 2020 - 1399/04/18");
+        }
+    }
+}
